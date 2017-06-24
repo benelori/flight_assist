efb6ff6d1d426b310a1b13ceb5a17e8969072c24
Bene Lorant <bene.lori89@gmail.com>
2017-06-24 22:39:29 +0300

    Implement About us Footer.

---
+++ web/modules/custom/aa_pages/src/Plugin/Block/AboutUsFooterBlock.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\aa_pages\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+
+/**
+ * Provides a 'AboutUsFooterBlock' block.
+ *
+ * @Block(
+ *  id = "about_us_footer_block",
+ *  admin_label = @Translation("About us footer block"),
+ * )
+ */
+class AboutUsFooterBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManager definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+  /**
+   * Constructs a new AboutUsFooterBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+        array $configuration,
+        $plugin_id,
+        $plugin_definition,
+        EntityTypeManager $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $storage = $this->entityTypeManager->getStorage('node');
+    $nids = $storage->getQuery()
+      ->condition('field_page_type', 'about_us')
+      ->execute();
+    if ($nids) {
+      /** @var NodeInterface $node */
+      $node = $storage->load(reset($nids));
+
+      $build = [
+        '#theme' => 'about_us',
+        '#about_us_node' => $this->entityTypeManager->getViewBuilder('node')->view($node, 'about_us_footer'),
+      ];
+    }
+
+    return $build;
+  }
+
+}
